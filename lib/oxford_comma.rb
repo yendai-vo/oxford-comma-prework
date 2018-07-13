44afca2618a1247571c33bdbc031a31f049ee5e3
Cindy Vo <yendai.vo@hotmail.com>
2018-07-13 01:01:46 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif(array.length == 2)
     array.join(" and ")
   elsif(array.length == 3)
-    array[0..-2].join(", ") + " and" + array[-1]
+    array[0..-2].join(", ") + ", and " + array[-1]
   end
 end
