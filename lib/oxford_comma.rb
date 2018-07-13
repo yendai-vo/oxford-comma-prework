ccca573368ac713c0ff3693e2d11987bd065ac6d
Cindy Vo <yendai.vo@hotmail.com>
2018-07-13 01:04:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if(array.length == 1)
+    array.first
+  elsif(array.length == 2)
+    array.join(" and ")
+  elsif(array.length == 3)
+    array[0..-2].join(", ") + ", and " + array[-1]
+  else
+    array[0..-2].join(", ") + ", and " + array[-1]
+  end
 end
