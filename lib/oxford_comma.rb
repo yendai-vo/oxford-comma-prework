60db7373b08269ca4b1fa6a74ecfc52a5cf480fc
Cindy Vo <yendai.vo@hotmail.com>
2018-07-13 01:03:39 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,6 @@ def oxford_comma(array)
   elsif(array.length == 3)
     array[0..-2].join(", ") + ", and " + array[-1]
   else
-    array[0..array.length].join(", ") + ", and " + array.last
+    array[0..(array.length -1)].join(", ") + ", and " + array.last
   end
 end
