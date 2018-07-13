7ab282a8d0a26a76145e3a0c86fbf4ab8fd27aa9
Cindy Vo <yendai.vo@hotmail.com>
2018-07-13 00:47:16 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,9 @@
 def oxford_comma(array)
   if(array.length == 1)
-    array.join(", ")
+    array.first
   elsif(array.length == 2)
     array.join(" and ")
-
+  elsif(array.length == 3)
+    
   end
 end
